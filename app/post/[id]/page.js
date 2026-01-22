0a0296c7dfe53b779920f836d3ef5d75469da958
Васильев Алексей Леонидович <alexeyvasiliev98@gmail.com>
2026-01-22 17:14:20 +0700

    метадата

--- app/post/[id]/page.js
+++ app/post/[id]/page.js
@@ -1,5 +1,14 @@
 import Post from "@/app/components/Post";
 
+export async function generateMetadata({ params, searchParams }) {
+  const { id } = await params;
+  const post = await fetchData(id);
+  return {
+    title: post.title + "- Статья на сайте",
+    description: post.body,
+  };
+}
+
 async function fetchData(id) {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts/" + id);
   const result = await res.json();
